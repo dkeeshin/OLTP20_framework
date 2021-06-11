8fbde3442b81780a95ee4dc943c8b299739c0052
dkeeshin <62847782+dkeeshin@users.noreply.github.com>
2021-06-11 16:56:14 -0500

    message_client code

---
+++ message_client/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/lib/pq"
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func waitForNotification(l *pq.Listener) {
+	for {
+		select {
+		case n := <-l.Notify:
+			fmt.Println("Received data from channel [", n.Channel, "] :")
+			// Prepare notification payload
+			var foo bytes.Buffer
+			err := json.Indent(&foo, []byte(n.Extra), "", "\t")
+			if err != nil {
+				fmt.Println("Error processing JSON: ", err)
+				return
+			}
+			fmt.Println("Send To gRPC...")
+			grpc_connect(foo.String())
+			return
+		case <-time.After(90 * time.Second):
+			fmt.Println("Received no events for 90 seconds, checking connection")
+			go func() {
+				l.Ping()
+			}()
+			return
+		}
+	}
+}
+
+func grpc_connect(message string) {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewGreeterClient(conn)
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: message})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Server Message Sent : %s", r.GetMessage())
+}
+
+func main() {
+
+	var conninfo string = "dbname=oltp20_framework host=localhost user=postgres port=5433 password=6765!linuX"
+	conn, err := pgx.Connect(context.Background(), conninfo)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	reportProblem := func(ev pq.ListenerEventType, err error) {
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+	listener := pq.NewListener(conninfo, 0*time.Second, time.Minute, reportProblem)
+	err = listener.Listen("events")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Listening for PostgreSQL Notifications...")
+	for {
+		waitForNotification(listener)
+	}
+}
