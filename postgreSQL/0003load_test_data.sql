e17e1187da5f257ffd7c52ebf5aee94f21cb4e25
kDSmedia <kdsmedia@video.keeshinmedia.com>
2021-07-07 11:24:59 -0500

    update 20210707 11:25

---
+++ postgreSQL/0003load_test_data.sql
@@ -0,0 +1,7 @@
+BEGIN TRANSACTION;
+INSERT INTO reference.location (name, latitude, longitude) VALUES ('Evanston, IL USA', '42.052158', '-87.687866');
+INSERT INTO reference.location (name, latitude, longitude) VALUES ('Lincolnshire, IL USA', '42.1900249', '-87.9084039')
+INSERT INTO reference.location (name, latitude, longitude) VALUES ('Chicago, IL USA', '41.897217','-87.6210423');
+COMMIT TRANSACTION;
+
+
